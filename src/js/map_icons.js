6fea233eefa4733b46f51b3e72d6745867eed88d
unknown <e.roderick@uqconnect.edu.au>
2022-10-20 13:30:38 +1000

    Added bus stop marker

--- src/js/map_icons.js
+++ src/js/map_icons.js
@@ -5,9 +5,10 @@
 /* Constants */
 const ICON_PLAYER_WIDTH = 56;
 const ICON_PLAYER_HEIGHT = 75;
+const ICON_STOP_SIZE = 30;
 
 /* Icons */
-var playerIcon = L.icon({
+const playerIcon = L.icon({
     iconUrl: "/DECO1800-7180/public/assets/avatar/player.svg",
     // SPecify size of the icon
     iconSize: [ICON_PLAYER_WIDTH, ICON_PLAYER_HEIGHT],
@@ -15,4 +16,11 @@ var playerIcon = L.icon({
     iconAnchor: [ICON_PLAYER_WIDTH/2, ICON_PLAYER_HEIGHT/2],
     // Specify where in the icon popup's should originate (rel to iconAnchor)
     popupAnchor: [-3, -76]
+});
+
+const busStop = L.icon({
+    iconUrl: "/DECO1800-7180/public/assets/ui/icons/ic_bus_stop.svg",
+    iconSize: [ICON_STOP_SIZE, ICON_STOP_SIZE],
+    iconAnchor: [ICON_STOP_SIZE/2, ICON_STOP_SIZE/2],
+    popupAnchor: [ICON_STOP_SIZE/2, ICON_STOP_SIZE/2],
 });
