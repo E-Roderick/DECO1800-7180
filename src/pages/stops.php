c94e3efedd6941f4c246703d3005b227db2d630f
unknown <e.roderick@uqconnect.edu.au>
2022-10-22 15:49:25 +1000

    Initial bus stop page

---
+++ src/pages/stops.php
@@ -0,0 +1,67 @@
+<?php include("../components/header_default.php"); ?>
+
+<main>
+<section class="container">
+    <article class="sub_page">
+        <a href="#!" class="back_button"> &lt; Back</a>
+    </article>
+    <section class="wrapper main-features">
+        <img src="/DECO1800-7180/public/assets/ui/icons/ic_outline_nearby_bus_stops.svg" alt="">
+        <h4>SELECT A NEARBY 61 BUS STOP</h4>
+        <div id="slider-wrapper">
+        <div class="inner-wrapper">
+            <input checked type="radio" name="slide" class="control" id="Slide1" />
+            <label for="Slide1" id="s1"></label>
+            <input type="radio" name="slide" class="control" id="Slide2" />
+            <label for="Slide2" id="s2"></label>
+            <input type="radio" name="slide" class="control" id="Slide3" />
+            <label for="Slide3" id="s3"></label>
+            <input type="radio" name="slide" class="control" id="Slide4" />
+            <label for="Slide4" id="s4"></label>
+            <div class="overflow-wrapper">
+            <div class="slide" href="#">
+                <ul>
+                <li><a href="#">Roma  Street Busway Station</a></li>
+                <li><a href="#">King George Square Station</a></li>
+                <li><a href="#">Cultural Centre Station</a></li>
+                <li><a href="#">South Bank busway Station</a></li>
+                <li><a href="#">Mater Hill Station</a></li>
+                </ul>
+                </div>
+            <div class="slide" href="#">
+                <ul>
+                <li><a href="#">Roma  Street Busway Station</a></li>
+                <li><a href="#">King George Square Station</a></li>
+                <li><a href="#">Cultural Centre Station</a></li>
+                <li><a href="#">South Bank busway Station</a></li>
+                <li><a href="#">Mater Hill Station</a></li>
+                </ul>
+                </div>
+            <div class="slide" href="#">
+                <ul>
+                <li><a href="#">Roma  Street Busway Station</a></li>
+                <li><a href="#">King George Square Station</a></li>
+                <li><a href="#">Cultural Centre Station</a></li>
+                <li><a href="#">South Bank busway Station</a></li>
+                <li><a href="#">Mater Hill Station</a></li>
+                </ul>
+                </div>
+            <div class="slide" href="#">
+                <ul>
+                <li><a href="#">Roma  Street Busway Station</a></li>
+                <li><a href="#">King George Square Station</a></li>
+                <li><a href="#">Cultural Centre Station</a></li>
+                <li><a href="#">South Bank busway Station</a></li>
+                <li><a href="#">Mater Hill Station</a></li>
+                </ul>
+                </div>
+            </div>
+        </div>
+        </div>
+
+        <input class="select_stop btn" type="submit" value="NEXT STEP">
+    </section>
+</section>
+</main>
+
+<?php include("../components/footer_default.php"); ?>
