57f93f77f17ab5090a93d074880b3628aa17a7de
unknown <e.roderick@uqconnect.edu.au>
2022-10-25 13:00:27 +1000

    Working back button

--- src/components/nav.php
+++ src/components/nav.php
@@ -1,6 +1,7 @@
 <?php
 
-function BackButton($url) {
+function BackButton() {
+    $url = $_SERVER['HTTP_REFERER'] ? $_SERVER['HTTP_REFERER'] : "/DECO1800-7180/";
     echo '
         <nav class="sub_page">
             <a href="'.$url.'" class="back_button"> &lt; Back</a>
